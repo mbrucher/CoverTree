29a539b721c4cfdcfa86034441547a9a3731d612
Matthieu Brucher <matthieu.brucher@gmail.com>
2012-03-12 21:58:11 +0100

    Basic Commit

---
+++ cover_tree.h
@@ -0,0 +1,16 @@
+/**
+ * \file cover_tree.h
+ */
+
+#ifndef COVERTREE_H
+#define COVERTREE_H
+
+/**
+ * Cover tree class, templated with the inner type
+ */
+template<class DataType>
+class CoverTree
+{
+};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,14 @@
+/**
+ * \file main.cpp
+ */
+
+#include <iostream>
+
+#include "cover_tree.h"
+
+int main(int argc, char** argv)
+{
+  CoverTree<float> tree;
+
+  return EXIT_SUCCESS;
+}
