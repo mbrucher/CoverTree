0fcd052630aa15eaa681331febc2cc5c861fff32
Matthieu Brucher <matthieu.brucher@gmail.com>
2012-03-17 21:43:34 +0100

    Added data test creation

--- main.cpp
+++ main.cpp
@@ -6,8 +6,13 @@
 #include <vector>
 
 #include <boost/random/mersenne_twister.hpp>
+#include <boost/random/normal_distribution.hpp>
+#include <boost/random/variate_generator.hpp>
+#include <ctime>
 
 #include "cover_tree.h"
+
+const long POINTSIZE = 2;
 
 float euclidian(const std::vector<float>& p1, const std::vector<float>& p2)
 {
@@ -17,6 +22,27 @@ float euclidian(const std::vector<float>& p1, const std::vector<float>& p2)
     dist += (p1[i] - p2[i]) * (p1[i] - p2[i]);
   }
   return dist;
+}
+
+std::vector<std::vector<float> > generate(long size)
+{
+  std::vector<std::vector<float> > data;
+
+  boost::mt19937 engine(static_cast<long unsigned int>(clock()));
+  boost::normal_distribution<double> generator;
+  boost::variate_generator<boost::mt19937, boost::normal_distribution<double> > binded(engine, generator);
+
+  for(long i = 0; i < size; ++i)
+  {
+    std::vector<float> point;
+    for(long j = 0; j < POINTSIZE; ++j)
+    {
+      point.push_back(binded());
+    }
+    data.push_back(point);
+  }
+
+  return data;
 }
 
 int main(int argc, char** argv)
